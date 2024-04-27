b3355b0a83dd009d04affcf9f9977bb70eb7d3f5
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-27 18:16:12 -0400

    update navbar styling; add logo pic

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ function App() {
     <div className="App">
       <Router>
         <NavBar />
-
         <BookList />
 
       </Router>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import '../styles/NavBar.scss';
+import logo from '../assets/logo.png';
 
 const NavBar = () => {
   return (
@@ -13,9 +14,8 @@ const NavBar = () => {
             window.scrollTo(0, 0);
           }}
         >
-          {/* create a store logo later */}
-          {/* <img />  */}
-          <p>A Little Bookstore</p>
+          <img src={logo} alt="logo"/>
+          {/* <p>A Little Bookstore</p> */}
         </Link>
 
         {/* will link to a popup page later for adding new book */}
