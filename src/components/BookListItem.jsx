d4b685ac7f754b1cd5b36debc620e9fceeb876df
lorraineC26 <lorraine.quinyee@gmail.com>
2024-04-28 22:49:13 -0400

    add conditional rednering for empty books array situation

--- src/components/BookListItem.jsx
+++ src/components/BookListItem.jsx
@@ -4,41 +4,53 @@ import "../styles/BookListItem.scss";
 
 import { selectedBook, deleteBook } from "../redux/actions/bookActions";
 
-
 const BookListItem = () => {
   const books = useSelector((state) => state.allBooks.books);
   const dispatch = useDispatch();
 
-  const renderList = books.map((book) => {
-    const { id, title, price, category, image } = book;
-    
-    const handleClickModalOpen = (book) => {dispatch(selectedBook(book));};
+  const renderList = () => {
+    // make sure the map fx only works when books array is not empty
+    if (books.length === 0) {
+      return <div className="empty-message">Oops, it's empty!</div>;
+    } else {
+      return books.map((book) => {
+        const { id, title, price, category, image } = book;
 
-    const handleDelete = (book) => {dispatch(deleteBook(book))};
+        const handleClickModalOpen = (book) => {
+          dispatch(selectedBook(book));
+        };
 
-    return (
-      <div className="book-list-item" key={id}>
-        <div className="book-card">
-          <div className="book-cover" onClick={() => handleClickModalOpen(book)}>
-            <img src={image} alt={title} />
+        const handleDelete = (book) => {
+          dispatch(deleteBook(book));
+        };
+
+        return (
+          <div className="book-list-item" key={id}>
+            <div className="book-card">
+              <div
+                className="book-cover"
+                onClick={() => handleClickModalOpen(book)}
+              >
+                <img src={image} alt={title} />
+              </div>
+
+              <div className="book-info">
+                <div className="title">{title}</div>
+                <div className="price">${price}</div>
+                <div className="category">{category}</div>
+              </div>
+
+              <button className="delete" onClick={() => handleDelete(book.id)}>
+                Delete
+              </button>
+            </div>
           </div>
+        );
+      });
+    }
+  };
 
-          <div className="book-info">
-            <div className="title">{title}</div>
-            <div className="price">${price}</div>
-            <div className="category">{category}</div>
-          </div>
-
-          <button className="delete" onClick={() => handleDelete(book.id)}>Delete</button>
-        </div>
-      </div>
-    );
-  })
-
-  return (
-    <>{renderList}</>
-  )
-  
+  return <>{renderList()}</>;
 };
 
 export default BookListItem;
